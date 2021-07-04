9360e5352c8d40f49ec0a25132a44f8ce8dfe284
bhawya lohia <bhawya.lohia45@gmail.com>
2021-07-05 00:29:34 +0530

    fix error in auth.js and buyer.js

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,13 @@ const router=express.Router();
 const admin=require("../firebase/index.js");
 const buyerCollection=require("../models/buyerCollection.js");
 const sellerCollection=require("../models/sellerCollection.js");
+
+
 const authCheck=(req,res,next)=>
 {
      console.log(req.headers.authtoken);
+     if(req.headers&&req.headers.authtoken)
+     {
      admin.auth().verifyIdToken(req.headers.authtoken)
      .then((user) => {
           req.user=user;
@@ -13,24 +17,44 @@ const authCheck=(req,res,next)=>
      })
      .catch((error) => {
        console.log("error",error.message);
-       res.send("404");
+       res.status(401).json({message:"user unauthorised"});
      });
+    }
+    else
+    {
+      res.status(400).json({message:"bad request"});
+    }
 }
+
+
 const createBuyer=(req,res,next)=>
 {
   console.log("reached createorupdateuser");
   const user=req.body;
+  if(user)
+  {
   buyerCollection.findOne({email:user.email})
   .then((userInDb)=>{
-   if(userInDb){res.send("user already exists");}
+   if(userInDb){res.json("user already exists");}
    else
    { const newUser= new buyerCollection(req.body);
      newUser.save()
-     .then((savedUser)=>{res.json(savedUser)})
-     .catch((err)=>{console.log(error)})
+     .then((savedUser)=>{res.json("success")})
+     .catch((error)=>{
+       console.log("buyer could not be saved to database:",error.message);
+       res.status(500).json({message:"internal server error. please try again."});
+    })
     }
    })
-   .catch((error)=>{console.log(error.message);});
+   .catch((error)=>{
+    console.log("error in finding buyer in database:",error.message);
+    res.status(500).json({message:"internal server error. please try again."});
+  });
+  }
+  else
+  {
+    res.status(500).json({message:"internal server error"});
+  }
 }
 
 const updateBuyer=(req,res,next)=>
@@ -42,23 +66,33 @@ const createProfessional=(req,res,next)=>
 {
   console.log("reached createorupdateuser");
   const user=req.body;
+  if(user&&user.email)
+  {
   sellerCollection.findOne({email:user.email})
   .then((userInDb)=>{
    if(userInDb)
    {
-     res.send("user already exists");
+     res.json("user already exists");
    }
    else
    {
       const newUser= new sellerCollection(req.body);
-      newUser.save();  
+      newUser.save().then((savedUser)=>{res.json("success")})
+      .catch((error)=>{console.log("error in saving user:",error); 
+      res.status(500).json({message:"internal server error. please try again."});
+    });  
    }
-   res.send("get done");
    })
    .catch((error)=>
    {
-      console.log(error.message);
+    console.log("error in finding buyer in database:",error.message);
+    res.status(500).json({message:"internal server error. please try again."});
    });
+  }
+  else
+  {
+    res.status(500).json({message:"internal server error"});
+  }
 }
 
 const updateProfessional=(req,res,next)=>
@@ -68,23 +102,34 @@ const updateProfessional=(req,res,next)=>
 
 const readUser=(req,res,next)=>
 {
-     buyerCollection.findOne({email:req.user.email})
-     .then((userInDb)=>{
-         if(userInDb)
-         {
-          res.json(userInDb);
-         }
-         else {
-            sellerCollection.findOne({email:req.user.email})
-             .then((userInDb)=>{
-                if(userInDb)
-                res.json(userInDb);
-                else {res.send("please register first")} 
-              })
-              .catch((error)=>{console.log(error)});
-          } 
-    })
-    .catch((error)=>{console.log(error)});
+     if(req.user&&req.user.email)
+     {
+        buyerCollection.findOne({email:req.user.email})
+        .then((userInDb)=>{
+            if(userInDb)
+            {
+              res.json(userInDb);
+            }
+            else {
+                sellerCollection.findOne({email:req.user.email})
+                .then((userInDb)=>{
+                    if(userInDb)
+                    res.json(userInDb);
+                    else {res.status(404).json({message:"user not found"})} 
+                  })
+                  .catch((error)=>{console.log(error);
+                    res.status(500).json({message:"internal server error. please try again."});
+                  });
+              } 
+        })
+        .catch((error)=>{console.log(error);
+          res.status(500).json({message:"internal server error. please try again."});
+        });
+    }
+    else
+    {
+      res.status(500).json({message:"internal server error"});
+    }
 };
 
 
--- routes/buyer.js
+++ routes/buyer.js
@@ -10,19 +10,28 @@ const hallServiceCollection=require("../models/hallServiceCollection");
 
 const authCheck=(req,res,next)=>
 {
-     console.log(req.headers.authtoken);
-     admin.auth().verifyIdToken(req.headers.authtoken)
-     .then((user) => {
-          req.user=user;
-          next();
-     })
-     .catch((error) => {
-       console.log("error",error.message);
-       res.send("404");
-     });
+    console.log(req.headers.authtoken);
+    if(req.headers&&req.headers.authtoken)
+    {
+    admin.auth().verifyIdToken(req.headers.authtoken)
+    .then((user) => {
+         req.user=user;
+         next();
+    })
+    .catch((error) => {
+      console.log("error",error.message);
+      res.status(401).json({message:"user unauthorised"});
+    });
+   }
+   else
+   {
+     res.status(400).json({message:"bad request"});
+   }
 }
 const checkBuyer=(req,res,next)=>
 {
+    if(req.user&&req.user.email)
+    {
      buyerCollection.findOne({email:req.user.email})
      .then((userInDb)=>
      {
@@ -31,12 +40,19 @@ const checkBuyer=(req,res,next)=>
              req.user=userInDb;
              next();
           }
-          else res.send("can't complete process. first login/register as a buyer");
+          else res.staus(403).json({message:"register as a buyer."});
      })
      .catch((error)=>{console.log(error)})
+    }
+    else
+    {
+        res.status(500).json({message:"internal server error"});
+    }
 }
 const addToCart=(req,res,next)=>
 {
+    if(req.body&&req.user&&req.user.email)
+    {
     const product=req.body;
     //const productType=(product.by.role==="Caterer"?"Cateringservices":(product.by.role==="Dj"?"Djservices":"Hallservices"));
      buyerCollection.findOne({email:req.user.email})
@@ -47,18 +63,27 @@ const addToCart=(req,res,next)=>
            {
             buyerInDb.cart.push(product._id);
             buyerInDb.save()
-            .then((savedBuyer)=>{console.log(savedBuyer)})
-            .catch((error)=>{console.log(error)})
+            .then((savedBuyer)=>{console.log(savedBuyer);
+                res.json("success");
+            })
+            .catch((error)=>{console.log(error);
+               res.status(500).json({message:"internal server error"});
+            })
            }
-           else res.send("already exists");
+           else res.json("already exists");
          }
-         else res.json("please register as a buyer");
+         else res.status(403).json({message:"register as a buyer"});
      })
-     .catch((error)=>{console.log(error)})
-    
+     .catch((error)=>{console.log(error);
+        res.status(500).json({message:"internal server error"});
+    });
+    }
+    else res.status(400).json({message:"bad request"});
 }
 const deleteFromCart=(req,res,next)=>
 {
+    if(req.body&&req.user&&req.user.email)
+    {
     const product=req.body;
     //const productType=(product.by.role==="Caterer"?"Cateringservices":(product.by.role==="Dj"?"Djservices":"Hallservices"));
      buyerCollection.findOne({email:req.user.email})
@@ -70,14 +95,22 @@ const deleteFromCart=(req,res,next)=>
            {
             buyerInDb.cart.splice(elementIndex,1);
             buyerInDb.save()
-            .then((savedBuyer)=>{console.log(savedBuyer)})
-            .catch((error)=>{console.log(error)})
+            .then((savedBuyer)=>{console.log(savedBuyer);
+                res.json("success");
+            })
+            .catch((error)=>{console.log(error);
+                res.status(500).json({message:"internal server error"});
+            })
            }
-           else res.send("does not exist.");
+           else res.json("does not exist.");
          }
-         else res.json("please register as a buyer");
+         else res.status(403).json("please register as a buyer");
      })
-     .catch((error)=>{console.log(error)})
+     .catch((error)=>{console.log(error);
+        res.status(500).json({message:"internal server error"});
+    });
+    }
+    else res.status(400).json({message:"bad request"});
 }
 const readOrders=(req,res,next)=>
 {
@@ -94,11 +127,15 @@ const readCart=(req,res,next)=>
         console.log(services, typeof services);
         res.json(services);
     })
-    .catch((error)=>{console.log(error)})
+    .catch((error)=>{console.log(error);
+        res.status(400).json({message:"bad request"});
+    })
 }
 
 const getServicesFromCart=async(buyer)=>
 {
+    if(buyer&&buyer.cart)
+    {
     var services=[];
     for(var i=0;i<buyer.cart.length;i++)
     {
@@ -108,18 +145,16 @@ const getServicesFromCart=async(buyer)=>
         currService=await djServiceCollection.findOne({_id:buyer.cart[i]}).populate('by');
         if(!currService)
         currService=await cateringServiceCollection.findOne({_id:buyer.cart[i]}).populate('by');
-        if(!currService)
-        {
-            throw new Error("inconsistency!!");
-        }
+        if(currService)
         services.push(currService);
     }
-    
-    return services;
+   }
+   else throw new Error("bad request");
+   return services;
 }
 
 
-router.post("/addtocart",authCheck,addToCart);
+router.post("/addtocart",authCheck,checkBuyer,addToCart);
 router.post("/cart",authCheck,checkBuyer,readCart);
 router.post("/removefromcart",authCheck,deleteFromCart);
 
